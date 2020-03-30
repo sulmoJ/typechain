a1c7abb4cf6b9804b0eff80482f01bb6a17d4b33
SungMo <tjdah0853@gmail.com>
2020-03-30 16:58:56 +0900

    ts 완료
    간단한 블록체인과 함께 완성 자바 class 구조와 좀더 가까워진 느낌의 js 인것
    같음

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,124 @@
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
-  constructor(name: string, age: number, gender: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+import * as CryptoJS from "crypto-js";
+
+class Block {
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+  static validateStructure = (aBlock: Block): boolean =>
+    typeof aBlock.index === "number" &&
+    typeof aBlock.hash === "string" &&
+    typeof aBlock.previousHash === "string" &&
+    typeof aBlock.timestamp === "number" &&
+    typeof aBlock.data === "string";
+
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
 
-const sulmo = new Human("sulmo", 24, "male");
-
-const sayHi = (person: Human): string => {
-  const { name, age, gender } = person;
-  return `Hello ${name}, you are ${age}, you are a ${gender}`;
+const genesisBlock: Block = new Block(0, "20202020202", "", "Hello", 123456);
+let blockchain: Block[] = [genesisBlock];
+const getBlockchain = (): Block[] => blockchain;
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+const createNewBlock = (data: string): Block => {
+  const previousBlock: Block = getLatestBlock();
+  const newIndex: number = previousBlock.index + 1;
+  const newTimestamp: number = getNewTimeStamp();
+  const nextHash: string = Block.calculateBlockHash(
+    newIndex,
+    previousBlock.hash,
+    newTimestamp,
+    data
+  );
+  const newBlock: Block = new Block(
+    newIndex,
+    nextHash,
+    previousBlock.hash,
+    data,
+    newTimestamp
+  );
+  addBlock(newBlock);
+  return newBlock;
 };
 
-console.log(sayHi(sulmo));
+const getHashforBlock = (aBlock: Block): string =>
+  Block.calculateBlockHash(
+    aBlock.index,
+    aBlock.previousHash,
+    aBlock.timestamp,
+    aBlock.data
+  );
+
+const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
+  if (!Block.validateStructure(candidateBlock)) {
+    console.log("구조문제");
+    return false;
+  } else if (previousBlock.index + 1 !== candidateBlock.index) {
+    console.log("인덱싱문제");
+    return false;
+  } else if (previousBlock.hash !== candidateBlock.previousHash) {
+    console.log("이전해쉬랑 현재 후보에서 이전해쉬 차이 문제");
+    return false;
+  } else if (getHashforBlock(candidateBlock) !== candidateBlock.hash) {
+    console.log("후보 해쉬의 검증 문제");
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const addBlock = (candidateBlock: Block): void => {
+  if (isBlockValid(candidateBlock, getLatestBlock())) {
+    blockchain.push(candidateBlock);
+  } else {
+    console.log("검사 통과 못해서 추가안댐");
+  }
+};
+
+createNewBlock("second block");
+createNewBlock("third block");
+createNewBlock("fourth block");
+console.log(blockchain);
 
 export {};
+
+// class Human {
+//   public name: string;
+//   public age: number;
+//   public gender: string;
+//   constructor(name: string, age: number, gender: string) {
+//     this.name = name;
+//     this.age = age;
+//     this.gender = gender;
+//   }
+// }
+
+// const sulmo = new Human("sulmo", 24, "male");
+
+// const sayHi = (person: Human): string => {
+//   const { name, age, gender } = person;
+//   return `Hello ${name}, you are ${age}, you are a ${gender}`;
+// };
+
+// console.log(sayHi(sulmo));
