884ff441cc149f0292ddee26465a3eee9c0d2e46
SungMo <tjdah0853@gmail.com>
2020-03-30 15:05:43 +0900

    ts class
    ts에서의 class와 js class의 차이중 권한 부여에대해 설명
    ts는 일반적인 oop에있는 public 과 private이 존재함 js는 없음

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,21 @@
-
-
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "sulmo",
-  age: 24,
-  gender: "male"
-};
+const sulmo = new Human("sulmo", 24, "male");
 
 const sayHi = (person: Human): string => {
   const { name, age, gender } = person;
   return `Hello ${name}, you are ${age}, you are a ${gender}`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(sulmo));
 
 export {};
